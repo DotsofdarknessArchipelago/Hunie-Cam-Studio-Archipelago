0918a96e15646ca8002b3821b8a26beb870dfbbf
Tenebrosful <tenebrosful@proton.me>
2026-05-15 00:48:31 +0200

    Refactor option descriptions and display names
    Updated display names and descriptions for various options to improve clarity and consistency.

--- APWorld/huniecamstudio/Options.py
+++ APWorld/huniecamstudio/Options.py
@@ -29,61 +29,81 @@ class enabled_girls(OptionSet):
     default = valid_keys.copy()
 
 class shop_items(Range):
-    """number of archipelago items in the shop Note if there is not enough locations for items it will add shop locations to satisfy the locations needed"""
-    display_name = "shop items"
+    """
+    Number of archipelago items in the shop.
+    
+    *If there is not enough locations for items, it will add shop locations to satisfy the locations needed.*
+    """
+    display_name = "Shop items"
     range_start = 0
     range_end = 200
     default = 0
 
 class start_slots(Range):
-    """start the game wth X girl slots open"""
-    display_name = "starting slots"
+    """
+    Start the game with X girl slots open.
+    """
+    display_name = "Starting slots"
     range_start = 1
     range_end = 8
     default = 2
 
 class item_slots(Range):
-    """start the game wth X girl item slots open"""
-    display_name = "starting item slots"
+    """
+    Start the game with X girl item slots open.
+    """
+    display_name = "Starting item slots"
     range_start = 0
     range_end = 3
     default = 1
 
 class clean_start(Toggle):
-    """makes the starting girl not smoke or drink"""
-    display_name = "clean start"
+    """
+    Makes the starting girl not smoking or drinking.
+    """
+    display_name = "Clean start"
     default = True
 
 class filler_item(Range):
-    """how the filler item is handled by making them all either:
-    1:nothing items,
-    2:random non progression items"""
-    display_name = "filler item"
+    """
+    How the filler items are handled
+    
+    1 - **Nothing items** *Fillers wont do anything*
+    2 - **Non progression items** *Fillers will be non-progression items*
+    """
+    display_name = "Filler items"
     range_start = 1
     range_end = 2
     default = 2
 
 class filler_game(Choice):
-    """limits the amount of locations that can have a progression item in them
-    none: no limit on what locations can have useful/progression
-    shop: excludes shop locations from having useful/progression items
-    full: no location in this slot will have useful/progression items in it(NOTE may cause generation to fail depending on other game slots)
     """
-    display_name = "filler game"
+    Limits the amount of locations that can have a progression item in.
+    
+    - **None** *No limit on what locations can have useful/progression.*
+    - **Shop** *Excludes shop locations from having useful/progression items.*
+    - **Full** *No location in this slot will have useful/progression items in it.*
+        *(It might cause generation to fail depending on other game slots.)*
+    """
+    display_name = "Filler game"
     option_none = 0
     option_shop = 1
     option_full = 2
     default = 0
 
 class goal(Toggle):
-    """true will set the goal to be getting trophy's
-    false will set the goal to max out every girls style/talent"""
-    display_name = "force goal"
+    """
+    - **True** *Goal is to obtain a minimum trophy.*
+    - **False** *Goal is to max out every girls style and talent.*
+    """
+    display_name = "Goal"
     default = True
 
 class min_trophy(Choice):
-    """sets the minimum trophy that will complete goal"""
-    display_name = "min trophy"
+    """
+    Sets the minimum trophy that will complete the trophy goal.
+    """
+    display_name = "Min trophy"
     option_bronze= 0
     option_silver= 1
     option_gold= 2
@@ -92,8 +112,10 @@ class min_trophy(Choice):
     default = 0
 
 class force_goal(Toggle):
-    """forces the goal to be achieved in 21 ingame days from starting a new save (NOTE: this still means you need to wait till the trophy check on the 22 day)"""
-    display_name = "force goal"
+    """
+    Forces the goal to be achieved in 21 ingame days from starting a new save.
+    *This still means you need to wait till the trophy check on the 22 day.*"""
+    display_name = "Force goal"
     default = False
 
 @dataclass
